a0c4cd6ca109bad5779fc7bc21868c4f4d370b2a
Tatsuya Ishii <tishii2479@gmail.com>
2022-08-02 12:59:08 +0900

    wip

---
+++ doc2vec.py
@@ -0,0 +1,37 @@
+from gensim.models.doc2vec import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
+
+from toydata import create_toydata
+
+
+def main():
+    num_topic = 5
+    data_size = 10
+    (documents, _), _ = create_toydata(num_topic=num_topic, data_size=data_size)
+
+    # words：1文書ずつ、単語に分割したリスト
+    # tags：文書識別用のタグ情報（リストで指定でき、複数のタグをつけることも可能）
+    # for文1回あたりの入力イメージは[TaggedDocument([単語1,単語2,単語3,......],[文書タグ])]
+    trainings = [TaggedDocument(words=documents[i], tags=[
+                                str(i)]) for i in range(len(documents))]
+
+    # 学習
+    model = Doc2Vec(documents=trainings, dm=1, vector_size=100,
+                    window=8, min_count=10, workers=4, epochs=100)
+
+    # モデルの保存('hogehoge'というファイル名で保存)
+    model.save('weights/doc2vec.model')
+
+    # モデルのロード(既に学習済みのモデルがある場合)
+    # model = Doc2Vec.load('../model/hogehoge.model')
+
+    # 文書タグ0(1つ目の文書)と類似している文書を上から3件(topnで指定)取得し、文書タグと類似度のセットが返ってくる
+    for tag in ['0', '10', '20', '30']:
+        print(model.dv.most_similar(tag, topn=data_size))
+
+# 結果イメージ[(タグ, 類似度), ......]
+# [('10', 0.99978), ('2', 0.98553), ('8', 0.98123)]
+
+
+if __name__ == '__main__':
+    main()
---
+++ toydata.py
@@ -0,0 +1,45 @@
+from random import randint, choice, shuffle
+import torch
+
+
+def create_toydata(num_topic: int, data_size: int):
+    documents = []
+    words = []
+    key_words = [[] for _ in range(num_topic)]
+
+    for _ in range(1, 201):
+        s = ''
+        for _ in range(10):
+            s += chr(ord('a') + randint(0, 26))
+        words.append(s)
+
+    for i in range(num_topic):
+        for j in range(1, 11):
+            s = chr(ord('a') + i) * j
+            key_words[i].append(s)
+
+    for i in range(num_topic):
+        for _ in range(data_size):
+            doc = []
+            for _ in range(randint(30, 50)):
+                doc.append(choice(key_words[i]))
+            for _ in range(randint(150, 200)):
+                doc.append(choice(words))
+            shuffle(doc)
+            documents.append(doc)
+
+    for i in range(num_topic):
+        words += key_words[i]
+
+    word_embedding = torch.eye(len(words))
+
+    return (documents, None), (words, word_embedding)
+
+
+def create_labeled_toydata(num_topic: int):
+    (documents, _), (words, word_embedding) = create_toydata(num_topic)
+    labels = []
+    for i in range(num_topic):
+        for _ in range(5):
+            labels.append(i)
+    return (documents, labels), (words, word_embedding)
