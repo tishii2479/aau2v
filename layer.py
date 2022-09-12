59af882989a2cfcf0fc078e77b83d2b3cc65ed8f
Tatsuya Ishii <tishii2479@gmail.com>
2022-09-12 15:50:08 +0900

    implement PositionalEncoding

--- layer.py
+++ layer.py
@@ -1,4 +1,5 @@
 import collections
+import math
 from typing import List
 
 import numpy as np
@@ -106,3 +107,38 @@ class NegativeSampling(nn.Module):
 
         loss = (positive_loss + negative_loss) / 2
         return loss
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(
+        self, d_model: int, max_sequence_length: int, dropout: float = 0.1
+    ) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_sequence_length, d_model)
+        position = torch.arange(0, max_sequence_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+        print(position.shape, pe.shape, div_term.shape)
+        print(torch.sin(position * div_term))
+        # d_model needs to be a even number
+        assert d_model % 2 == 0
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        self.pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pe_const", self.pe)
+
+    def forward(self, hs: Tensor) -> Tensor:
+        """Add positional encoding
+
+        Args:
+            hs (Tensor):
+                size: (batch_size, sequence_length, d_model)
+
+        Returns:
+            Tensor: positional encoded hs
+        """
+        hs = hs + self.pe[: hs.size(0), :]
+        return self.dropout.forward(hs)
--- tests/test_layer.py
+++ tests/test_layer.py
@@ -1,7 +1,7 @@
 import unittest
 
 import torch
-from layer import EmbeddingDot, NegativeSampling
+from layer import EmbeddingDot, NegativeSampling, PositionalEncoding
 
 
 class TestLayer(unittest.TestCase):
@@ -12,8 +12,8 @@ class TestLayer(unittest.TestCase):
             [0, 2, 1],
         ]
         layer = NegativeSampling(
-            d_model=3, num_item=4, sequences=sequences, power=1,
-            negative_sample_size=5)
+            d_model=3, num_item=4, sequences=sequences, power=1, negative_sample_size=5
+        )
         h = torch.Tensor([[0, 1, 0]])
         target_index = torch.tensor([0], dtype=torch.long)
         _ = layer.forward(h, target_index)
@@ -35,6 +35,16 @@ class TestLayer(unittest.TestCase):
         self.assertAlmostEqual(out[1][0][0], embedding_weight[1][0])
         self.assertAlmostEqual(out[1][0][1], embedding_weight[2][0])
 
+    def test_positional_encoding(self) -> None:
+        batch_size = 2
+        d_model = 4
+        seq_length = 3
+        layer = PositionalEncoding(
+            d_model=d_model, max_sequence_length=seq_length, dropout=0.1
+        )
+        hs = torch.rand(batch_size, seq_length, d_model)
+        _ = layer.forward(hs)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
